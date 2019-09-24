38d886cd51fd8d8e2a26b7af275147651bde2dd3
PRO-BIT\janezs <janez.sedeljsak@gmail.com>
2019-09-24 13:29:23 +0200

    api fix

--- server/routes/get.js
+++ server/routes/get.js
@@ -7,7 +7,7 @@ class DBMethods {
     static getChatGroups() {
         return new Promise(async resolve => {
             const qb = new QueryBuilder(settings, 'mysql', 'single');
-    
+
             qb.select([
                 'g name'
             ]).from('groups g')
@@ -20,71 +20,57 @@ class DBMethods {
     static getGroupById(id) {
         return new Promise(async resolve => {
             const qb = new QueryBuilder(settings, 'mysql', 'single');
-    
+
             qb.select([
                 'g name'
             ]).from('groups g')
-                .where({'g.id': id})
+                .where({ 'g.id': id })
                 .get((err, result) => {
                     qb.disconnect();
                     resolve(result);
                 });
         });
     }
-    static getGroups() {
+    static getGroups(id) {
         return new Promise(async resolve => {
             const qb = new QueryBuilder(settings, 'mysql', 'single');
-    
+
             qb.select([
-                'c.id as id',
-                'CONCAT(t.name, " ", t.surname) AS teacher',
-                'c.name as competition',
-                'c.deadline',
-                'c.date_created',
-                'c.places',
-                'c.description'
-            ]).from('competitions c')
-                .join('teachers t', 't.id=c.teacher_id', 'left')
+                'g name'
+            ]).from('groups g')
                 .get((err, result) => {
                     qb.disconnect();
                     resolve(result);
                 });
         });
     }
+
     static getYourGroups() {
         return new Promise(async resolve => {
             const qb = new QueryBuilder(settings, 'mysql', 'single');
-    
+
             qb.select([
-                'c.id as id',
-                'CONCAT(t.name, " ", t.surname) AS teacher',
-                'c.name as competition',
-                'c.deadline',
-                'c.date_created',
-                'c.places',
-                'c.description'
-            ]).from('competitions c')
-                .join('teachers t', 't.id=c.teacher_id', 'left')
+                'g name'
+            ])
+                .from('groups g')
+                .where({ 'g.admin': id })
                 .get((err, result) => {
                     qb.disconnect();
                     resolve(result);
                 });
+
+
         });
     }
+
     static getPeople() {
         return new Promise(async resolve => {
             const qb = new QueryBuilder(settings, 'mysql', 'single');
-    
+
             qb.select([
-                'c.id as id',
-                'CONCAT(t.name, " ", t.surname) AS teacher',
-                'c.name as competition',
-                'c.deadline',
-                'c.date_created',
-                'c.places',
-                'c.description'
-            ]).from('competitions c')
-                .join('teachers t', 't.id=c.teacher_id', 'left')
+                'u.name',
+                'u.surname'
+            ]).from('users u')
                 .get((err, result) => {
                     qb.disconnect();
                     resolve(result);
@@ -97,14 +83,14 @@ router.get('/get-groups', async (req, res, next) => {
     res.status(200).json({
         ok: true,
         result: await DBMethods.getGroups()
-    })  
+    })
 });
 
 router.get('/get-people', async (req, res, next) => {
     res.status(200).json({
         ok: true,
         result: await DBMethods.getPeople()
-    })  
+    })
 });
 
 router.get('/get-your-groups', async (req, res, next) => {
@@ -118,7 +104,7 @@ router.post('/get-group-by-id', async (req, res, next) => {
     res.status(200).json({
         ok: true,
         result: await DBMethods.getGroupById(req.body.id)
-    })  
+    })
 });
 
 router.get('/get-chat-groups', async (req, res, next) => {
